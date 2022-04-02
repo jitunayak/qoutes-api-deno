54b560fb805175c545ee66da7c14e2027a16d696
jitunayak <jitunayak715@gmail.com>
2022-04-02 09:00:54 +0530

    initial commit

---
+++ bookmarks.api.ts
@@ -0,0 +1,26 @@
+import { Application, Router } from "https://deno.land/x/oak@v10.5.1/mod.ts";
+import { quotes } from "./quotes.ts";
+const port = Deno.env.get("PORT") || 8080;
+const app = new Application();
+const router = new Router();
+
+router.get("/", (ctx) => {
+  ctx.response.body = quotes.get(Math.floor(Math.random() * 10));
+});
+
+router.get("/:id", (ctx) => {
+  if (quotes?.has(+ctx?.params?.id)) {
+    ctx.response.body = quotes.get(+ctx.params.id);
+  }
+});
+
+router.get("/all", (ctx) => {
+  ctx.response.body = Array.from(quotes.entries());
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`🌳 oak server running at port ${port} 🌳`);
+
+await app.listen({ port: +port });
---
+++ quotes.ts
@@ -0,0 +1,63 @@
+export const quotes = new Map<number, string>();
+
+quotes.set(0, "Are you a programmer or a coder ?");
+
+quotes.set(
+  1,
+  `“Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.”  Patrick McKenzie
+`
+);
+quotes.set(
+  2,
+  `“No one in the brief history of computing has ever written a piece of perfect software. It’s unlikely that you’ll be the first.” – Andy Hunt
+
+`
+);
+
+quotes.set(
+  3,
+  `“It’s hard enough to find an error in your code when you’re looking for it; it’s even harder when you’ve assumed your code is error-free.” – Steve McConnell`
+);
+
+quotes.set(
+  4,
+  `“Good code is its own best documentation. As you’re about to add a comment, ask yourself, “How can I improve the code so that this comment isn’t needed?” Improve the code and then document it to make it even clearer.” – Steve McConnell`
+);
+
+quotes.set(
+  5,
+  `“The computer was born to solve problems that did not exist before.” – Bill Gates 
+`
+);
+
+quotes.set(
+  6,
+  `Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.
+
+`
+);
+quotes.set(
+  7,
+  `Pasting code from the Internet into production code is like chewing gum found in the street.
+
+`
+);
+
+quotes.set(
+  8,
+  `Did you know? The collective noun for a group of programmers is a merge-conflict.
+
+`
+);
+
+quotes.set(
+  9,
+  `The secret of getting ahead is getting started.
+`
+);
+
+quotes.set(
+  10,
+  `Before you marry a person you should first make them use a computer with slow Internet to see who they really are
+`
+);
