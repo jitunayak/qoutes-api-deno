2ccf31eff98e7a356c16d43e3957b4beae513254
Jitu Nayak <jitunayak715@gmail.com>
2023-03-23 09:45:36 +0530

    Update bookmarks.api.ts

--- bookmarks.api.ts
+++ bookmarks.api.ts
@@ -4,17 +4,18 @@ const port = Deno.env.get("PORT") || 8080;
 const app = new Application();
 const router = new Router();
 
-router.get("/", (ctx) => {
-  ctx.response.body = quotes.get(Math.floor(Math.random() * quotes.size));
+router.get("/api/v1/", (ctx) => {
+  const index = Math.floor(Math.random() * quotes.size);
+  ctx.response.body = {id: index, quote: quotes.get(index)};
 });
 
-router.get("/:id", (ctx) => {
+router.get("/api/v1/:id", (ctx) => {
   if (quotes?.has(+ctx?.params?.id)) {
-    ctx.response.body = quotes.get(+ctx.params.id);
+    ctx.response.body = { id:ctx?.params?.id, quote:quotes.get(+ctx.params.id)};
   }
 });
 
-router.get("/all", (ctx) => {
+router.get("/api/v1/quotes", (ctx) => {
   ctx.response.body = Array.from(quotes.entries());
 });
 
